64f7ba0c5deeb9273655298c4cdf9d39cfb4e444
Miria Santos <miriaandressa1@gmail.com>
2021-02-26 17:51:55 -0300

    Exercicio para revisar

--- Java/Aulas/src/Exerc_IfElse/Ex2.java
+++ Java/Aulas/src/Exerc_IfElse/Ex2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ex2 {
 	public static void main(String[] args)
 	{
-		int num1, num2, num3;
+		int num1, num2, num3, maior, menor, meio;
 		
 		Scanner ler = new Scanner(System.in);
 		System.out.println("Informe o primeiro número: ");
@@ -15,20 +15,6 @@ public class Ex2 {
 		System.out.println("Informe o terceiro número: ");
 		num3=ler.nextInt();
 		
-		if (num1>num2 && num1>num3 && num2>num3) {			
-			System.out.println("A sequência crescente é "+num3+", "+num2+", "+num1);
-		} else if (num1<num2 && num2<num3 && num2<num3) {
-			System.out.println("A sequência crescente é "+num1+", "+num2+", "+num3);
-		} else if (num1<num2 && num2>num3 && num1>num3) {
-			System.out.println("A sequência crescente é "+num3+", "+num1+", "+num2);
-		} else if (num1<num2 && num2>num3 && num1<num3) {
-			System.out.println("A sequência crescente é "+num1+", "+num3+", "+num2);
-		} else if (num1>num2 && num2<num3 && num1<num3) {
-			System.out.println("A sequência crescente é "+num2+", "+num1+", "+num3);
-		} else if (num1>num2 && num2<num3 && num1>num3) {
-			System.out.println("A sequência crescente é "+num2+", "+num3+", "+num1);
-		} else {
-			System.out.println("A sequência é: "+num1+" "+num2+" "+num3);
-		}
+	
 	}
 }
